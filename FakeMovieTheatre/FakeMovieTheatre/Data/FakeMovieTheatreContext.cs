36d2ae639aa51e5700bc3e57849c07927b915b68
Tanner Percival <percivaltanner@gmail.com>
2019-08-12 10:44:58 -0700

    adds theatre files and initial razor page

---
+++ FakeMovieTheatre/FakeMovieTheatre/Data/FakeMovieTheatreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeMovieTheatre.Models
+{
+    public class FakeMovieTheatreContext : DbContext
+    {
+        public FakeMovieTheatreContext (DbContextOptions<FakeMovieTheatreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FakeMovieTheatre.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ FakeMovieTheatre/FakeMovieTheatre/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FakeMovieTheatre.Models;
+
+namespace FakeMovieTheatre.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FakeMovieTheatre.Models.FakeMovieTheatreContext _context;
+
+        public IndexModel(FakeMovieTheatre.Models.FakeMovieTheatreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
